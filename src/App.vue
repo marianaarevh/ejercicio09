9776371d74abaeb51dbd5c881b78d8323ddef1a0
marianaarevh <marianaarevalo933@gmail.com>
2023-11-18 23:44:32 -0700

    ejercicio 09 completo

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,45 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Calendario de Eventos</h1>
+  <EventCalendar :events="eventList" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import EventCalendar from '@/components/EventCalendar.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    EventCalendar
+  },
+  data() {
+    return {
+      eventList: [
+      {
+          id: 1,
+          title: "Reunión de equipo",
+          date: "2023-11-20",
+          description: "Discutir los progresos del proyecto y asignar tareas para la próxima semana."
+        },
+        {
+          id: 2,
+          title: "Conferencia de Desarrolladores",
+          date: "2023-11-25",
+          description: "Asistir a la conferencia anual de desarrolladores para aprender sobre las últimas tecnologías."
+        },
+        {
+          id: 3,
+          title: "Entrenamiento de Vue.js",
+          date: "2023-12-02",
+          description: "Participar en un curso de capacitación intensivo sobre Vue.js para mejorar habilidades de desarrollo."
+        },
+        {
+          id: 4,
+          title: "Lanzamiento de Producto",
+          date: "2023-12-10",
+          description: "Presentación del nuevo producto a clientes y socios importantes."
+        }
+      ]
+    }
   }
 }
 </script>
@@ -20,7 +50,19 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: black;
   margin-top: 60px;
 }
+
+body{
+  background-image: url("./assets/bg.jpg");
+}
+
+h1{
+    font-size: 2.2rem;
+    font-weight: 800;
+    color: black;
+    -webkit-text-stroke-width: 1px; 
+    -webkit-text-stroke-color: #ffffffcf; 
+  }
 </style>
---
+++ src/components/EventCalendar.vue
@@ -0,0 +1,35 @@
+<template>
+    
+    <div class="event-calendar">
+      <div v-for="event in events" :key="event.id" class="event">
+        <h3>{{ event.title }}</h3>
+        <p>{{ event.date }}</p>
+        <p>{{ event.description }}</p>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      events: Array
+    }
+  };
+  </script>
+  
+  <style scoped>
+
+  
+  .event-calendar {
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
+    gap: 20px;
+    margin: 30px;
+  }
+  
+  .event {
+    border: 1px solid #000000;
+    padding: 10px 20px;
+    background-color: #FFFF99;
+  }
+  </style>
